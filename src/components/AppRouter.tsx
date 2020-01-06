773ce7fef9df82fe98c144e9bae5c22b998b84d1
Lucas Rodrigues <l.rsouza@outlook.com>
2020-01-06 07:40:57 +0000

    feat(router): implement first version of react-router

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
+import Login from '../container/Login';
+import Main from '../container/Main';
+
+//TODO: improve this file using React Suspense.
+//https://itnext.io/async-react-using-react-router-suspense-a86ade1176dc
+const AppRouter: FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/main" component={Main} />
+        <Route path="/" exact component={Login} />
+        {/* <Route component={NotFound} /> */}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
