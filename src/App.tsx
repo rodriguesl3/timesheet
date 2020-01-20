cb64357e0b8db1c38ed2306710e8fa013a18d415
Lucas Rodrigues <l.rsouza@outlook.com>
2020-01-20 06:57:42 +0000

    feat: add redux sagas, simulate login request

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ErrorBoundary from './container/ErrorBoundary';
 import Router from './components/AppRouter';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <ErrorBoundary>
-        <Router />
-      </ErrorBoundary>
+      <Provider store={store}>
+        <ErrorBoundary>
+          <Router />
+        </ErrorBoundary>
+      </Provider>
     </div>
   );
 };
