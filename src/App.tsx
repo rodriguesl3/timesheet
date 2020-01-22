930a110deaf0196fa5e391bd5a94cd7c1fc28789
Lucas Rodrigues <l.rsouza@outlook.com>
2020-01-22 07:05:51 +0000

    feat(login): defined styled guide

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,21 @@ import Router from './components/AppRouter';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+import { ThemeProvider, darkTheme, lightTheme } from 'bold-ui';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
 const App: React.FC = () => {
   return (
     <div className="App">
+      <ToastContainer />
       <Provider store={store}>
         <ErrorBoundary>
-          <Router />
+          <ThemeProvider theme={lightTheme}>
+            <nav>THis is a navbar</nav>
+            <Router />
+          </ThemeProvider>
         </ErrorBoundary>
       </Provider>
     </div>
