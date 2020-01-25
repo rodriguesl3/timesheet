91dbcc7592e4818a8aede9f5f38b8d2e35357d05
Lucas Rodrigues <l.rsouza@outlook.com>
2020-01-25 20:47:54 +0000

    feat(main): add rechart, add card layout

---
+++ src/index.style.ts
@@ -0,0 +1,22 @@
+import styled, { StyledComponentBase } from 'styled-components';
+
+export interface CardProps {
+  width: number;
+}
+
+export const Card = styled.div<CardProps>`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 25px;
+  padding: 30px;
+  box-shadow: 0 0 15px 3px rgba(0, 0, 0, 0.5);
+  transition: 0.5s;
+  justify-content: center;
+  align-content: center;
+  width: ${({ width = 0 }): number => width}%;
+
+  &:hover {
+    box-shadow: 0 0 20px 10px rgba(0, 0, 0, 0.5);
+  }
+`;
--- src/pages/Main/main.style.ts
+++ src/pages/Main/main.style.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
-export const MainContainer = styled.div``;
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  width: 100%;
+`;
 
 export const ContentContainer = styled.div`
   display: flex;
   flex-direction: row;
   background-color: #e9e9e9;
+  width: 100%;
 `;
