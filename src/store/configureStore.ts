6a0983bc62f32c5d565efe255fb8a4eeafdb1226
Lucas Rodrigues <l.rsouza@outlook.com>
2020-01-29 07:07:04 +0000

    feat(main): create redux to consume stock symbols

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,10 +1,37 @@
 import createSagaMiddleware from 'redux-saga';
-import { createStore, applyMiddleware } from 'redux';
-import reducer from './Login/reducer';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import watchSagas from './configureSaga';
+import loginReducer from './Login/reducer';
+import sidebarReducer from './Sidebar/reducer';
+import projectReducer from './Projects/reducer';
+import { LoginState } from './Login/types';
+import { SideBarState } from './Sidebar/types';
+import { ProjectState } from './Projects/types';
+import { StockSymbolsState } from './StockSymbols/types';
+
+export interface BaseState {
+  success: boolean;
+  loading: boolean;
+  failure: boolean;
+}
+
+export interface GlobalState {
+  loginState: LoginState;
+  sidebarState: SideBarState;
+  projectState: ProjectState;
+  stockSymbolState: StockSymbolsState;
+}
+
+const globalState = combineReducers({
+  loginState: loginReducer,
+  projectState: projectReducer,
+  sidebarState: sidebarReducer,
+});
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const composeEnhancers = composeWithDevTools({});
+const store = createStore(globalState, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(watchSagas);
 
