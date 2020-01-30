f238df4bc0fc536071d5e46d7f2d5f8f1d938fe5
Lucas Rodrigues <l.rsouza@outlook.com>
2020-01-30 07:41:17 +0000

    feat(stocksymbols): request stock symbols

--- src/store/configureSaga.ts
+++ src/store/configureSaga.ts
@@ -1,9 +1,15 @@
-import { takeLatest, all } from 'redux-saga/effects';
+import { takeLatest, all, ForkEffect, AllEffect } from 'redux-saga/effects';
 import { LoginTypes } from './Login/types';
 import { load } from './Login/sagas';
 import { SideBarTypes } from './Sidebar/types';
 import { loadSideBar } from './Sidebar/sagas';
+import { StockSymbolsTypes } from './StockSymbols/types';
+import { loadStockSymbols } from './StockSymbols/sagas';
 
-export default function* watchSagas() {
-  return yield all([takeLatest(LoginTypes.LOGIN_REQUEST, load), takeLatest(SideBarTypes.SIDEBAR_REQUEST, loadSideBar)]);
+export default function* watchSagas(): Generator<AllEffect<object>> {
+  return yield all([
+    takeLatest(LoginTypes.LOGIN_REQUEST, load),
+    takeLatest(SideBarTypes.SIDEBAR_REQUEST, loadSideBar),
+    takeLatest(StockSymbolsTypes.STOCKSYMBOLS_REQUEST, loadStockSymbols),
+  ]);
 }
