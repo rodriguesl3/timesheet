4f88bba4bb6aff510289d95217ec66637f7fe674
Lucas Rodrigues <l.rsouza@outlook.com>
2020-02-02 18:55:35 +0000

    feat(wallet): add ducks foldter structure for wallet component

--- src/components/StockSymbols/StockSymbols.tsx
+++ src/components/StockSymbols/StockSymbols.tsx
@@ -1,18 +1,32 @@
-import React, { FC, useCallback, useEffect, useMemo, useRef, RefObject, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Select from 'react-select';
 
 import { StockSymbolsTypes } from '../../store/StockSymbols/types';
 import { GlobalState } from '../../store/configureStore';
+import { Button } from 'bold-ui';
+import { SymbolContainer } from './StockSymbols.style';
 
+/**
+ *
+ * https://jedwatson.github.io/react-select/
+ */
 const StockSymbols: FC = () => {
   const dispatch = useDispatch();
   const stocksSymbolsList = useSelector((state: GlobalState) => state.stockSymbolState);
   const [selected, setSelected] = useState();
 
-  const stockSymbolFiltered = (searchTerm: any): void => {
-    console.log(searchTerm);
+  const requestSymbol = (): void => {
+    console.log('clicked to search');
 
+    // dispatch(
+    //   {
+    //     type:
+    //   }
+    // )
+  };
+
+  const stockSymbolFiltered = (searchTerm: any): void => {
     dispatch({
       type: StockSymbolsTypes.STOCKSIMBOLS_FILTER,
       payload: searchTerm,
@@ -20,30 +34,31 @@ const StockSymbols: FC = () => {
   };
 
   const handleChange = (selectedOption: any) => {
-    setSelected(selectedOption);
+    setSelected([...selectedOption]);
     console.log(`Option selected:`, selected);
   };
 
   const renderList = (): object => {
     return (
-      <div>
+      <SymbolContainer>
         <Select
+          className="stock-symbol-select"
           value={selected}
           onInputChange={searchTerm => stockSymbolFiltered(searchTerm)}
+          isMulti={true}
           options={stocksSymbolsList.stockSymbolFiltered.map(elm => ({
             value: elm.symbol,
-            label: elm.companyName,
+            label: `${elm.symbol} - ${elm.companyName}`,
           }))}
           onChange={handleChange}
         />
-      </div>
+
+        <Button kind="primary" size="small">
+          Search
+        </Button>
+      </SymbolContainer>
     );
   };
-
-  // useEffect(() => {
-  //   const filter = listRef.current?.current?.value ?? '';
-  //   stockSymbolFiltered(filter);
-  // }, [listRef.current?.current?.value ?? '']);
 
   useEffect(() => {
     dispatch({
