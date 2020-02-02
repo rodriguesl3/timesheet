f5597fcb2f2d37fac4bfd0b8813dafd6d8ae8e11
Lucas Rodrigues <l.rsouza@outlook.com>
2020-02-02 18:55:35 +0000

    feat(wallet): add ducks foldter structure for wallet component

---
+++ src/components/StockSymbols/StockSymbols.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SymbolContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+
+  .stock-symbol-select {
+    width: 100%;
+  }
+`;
