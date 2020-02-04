57b9d37fd6321d6e8037e1f64066ecbcc7227fe9
Lucas Rodrigues <l.rsouza@outlook.com>
2020-02-04 22:47:42 +0000

    feat(wip_wallet): add redux and redux sagas structure

--- src/store/Wallet/action.ts
+++ src/store/Wallet/action.ts
@@ -1,1 +1,12 @@
-export const action = '';
+import { EmptyAction, PayloadAction, action } from 'typesafe-actions';
+import { Symbols } from 'recharts';
+import { WalletTypes, Company } from './types';
+
+export const walletRequest = (symbols: Symbols[]): PayloadAction<WalletTypes.WALLET_REQUEST, Symbols[]> =>
+  action(WalletTypes.WALLET_REQUEST, symbols);
+
+export const walletSuccess = (companies: Company[]): PayloadAction<WalletTypes.WALLET_SUCCESS, Company[]> =>
+  action(WalletTypes.WALLET_SUCCESS, companies);
+
+export const walletFailure = (error: object): PayloadAction<WalletTypes.WALLET_FAILURE, object> =>
+  action(WalletTypes.WALLET_FAILURE, error);
