a73b8e6470757116240f542b34da6a8d01195ecc
Lucas Rodrigues <l.rsouza@outlook.com>
2020-02-04 22:47:42 +0000

    feat(wip_wallet): add redux and redux sagas structure

--- src/store/Wallet/reducer.ts
+++ src/store/Wallet/reducer.ts
@@ -1,1 +1,25 @@
-export const reducer = '';
+import { WalletTypes, WalletState } from './types';
+import { Reducer } from 'redux';
+
+type WalletReducer = {
+  type: WalletTypes;
+  payload?: any;
+};
+
+const walletReducer: Reducer<WalletState> = (
+  state: WalletState = { companies: [], failure: false, loading: false, success: false },
+  action: WalletReducer,
+) => {
+  switch (action.type) {
+    case WalletTypes.WALLET_REQUEST:
+      return { ...state, failure: false, loading: true, success: false };
+    case WalletTypes.WALLET_FAILURE:
+      return { ...state, failure: true, loading: false, success: false };
+    case WalletTypes.WALLET_SUCCESS:
+      return { ...state, companies: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default walletReducer;
--- src/store/Wallet/sagas.ts
+++ src/store/Wallet/sagas.ts
@@ -1,1 +1,34 @@
-export const sagas = '';
+import { WalletTypes, Company } from './types';
+import { fullUrlBatchWalletInformation } from '../../services/constant';
+import { call, put } from 'redux-saga/effects';
+import api from '../../services';
+import { StockSymbol } from '../StockSymbols/types';
+import { walletSuccess, walletFailure } from './action';
+
+export type WalletSagas = {
+  type: WalletTypes;
+  payload: StockSymbol[];
+};
+
+function* parseCompany(response: any, symbols: string[]): Generator<Company[]> {
+  return symbols.map(
+    elm =>
+      ({
+        symbol: elm,
+        chart: response[elm]['chart'],
+        news: response[elm]['news'],
+        quote: response[elm]['quote'],
+      } as Company),
+  );
+}
+
+export function* loadWalletInformation(action: WalletSagas): object {
+  try {
+    const symbols = action.payload.map(elm => elm.symbol);
+    const apiResponse = yield call(api.get, fullUrlBatchWalletInformation(symbols));
+    const parseResponse = yield parseCompany(apiResponse, symbols);
+    yield put(walletSuccess(parseResponse));
+  } catch (err) {
+    yield put(walletFailure(err));
+  }
+}
