82d1d984646da01f187e0403bcec957cf6c23f8d
Lucas Rodrigues <l.rsouza@outlook.com>
2020-02-06 23:06:42 +0000

    feat(news-section): adding news from symbol requested

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -3,6 +3,9 @@ import { LineChart, Line, AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area 
 import { useSelector } from 'react-redux';
 import { ProjectState } from '../../store/Projects/types';
 import { GlobalState } from '../../store/configureStore';
+import { Company } from '../../store/Wallet/types';
+import { Tabs, TabItem } from 'bold-ui';
+import { TabContainerExtension } from './projects.style';
 
 /**
  * Rechart value: http://recharts.org/en-US/guide/getting-started
@@ -21,81 +24,26 @@ interface ProjectProps {
 }
 
 const Project: FC<ProjectProps> = (props: ProjectProps) => {
-  const projects: ProjectState = useSelector((state: GlobalState) => state.projectState);
-
-  // const parseGraphValue = () => {
-  //   //projects.projects.flatMap(proj => proj.evolution.map(evo => {[proj.name]: evo.value, [evo.name]:evo.value}))
-  //   projects.projects.map(proj => proj.evolution.map(evo => `{${proj.name}: ${evo.value}, name: ${evo.name}`));
-  // };
-
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+  //const projects: ProjectState = useSelector((state: GlobalState) => state.projectState);
+  const assets: Company[] = useSelector((state: GlobalState) => state.walletState.companies);
 
   return (
     <div>
       <h1>{props.title}</h1>
-      <p>{projects.projects && projects.projects.map(pro => pro.name)}</p>
-
-      <AreaChart width={400} height={250} data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-        <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#9f4cf1" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#9f4cf1" stopOpacity={0} />
-          </linearGradient>
-          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#2493ff" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#2493ff" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-        {/* <XAxis dataKey="name" />
-        <YAxis /> */}
-        {/* <CartesianGrid strokeDasharray="3 3" /> */}
-        {/* <Tooltip /> */}
-        <Area type="monotone" dataKey="uv" stroke="#9f4cf1" fillOpacity={1} fill="url(#colorUv)" />
-        <Area type="monotone" dataKey="pv" stroke="#2493ff" fillOpacity={1} fill="url(#colorPv)" />
-      </AreaChart>
+      <TabContainerExtension>
+        {assets.map((company: Company, idx: number) => (
+          <>
+            <TabItem key={idx} onClick={console.log}>
+              {company.symbol}
+            </TabItem>
+            <div className="news-container">
+              {company.news.map((news, indx: number) => (
+                <div key={indx}>{news.summary}</div>
+              ))}
+            </div>
+          </>
+        ))}
+      </TabContainerExtension>
     </div>
   );
 };
