79574dd8378294f2f29ddf3f744d68df742f5085
Lucas Rodrigues <l.rsouza@outlook.com>
2020-02-07 07:03:46 +0000

    style(news_section): (wip) styiling news section

--- src/components/Projects/projects.style.ts
+++ src/components/Projects/projects.style.ts
@@ -1,17 +1,7 @@
 import styled from 'styled-components';
-import { Tabs } from 'bold-ui';
 
-export const TabContainerExtension = styled(Tabs)`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  overflow: auto;
-  height: 100vh;
-
-  .news-container {
-    display: flex;
-    flex-direction: column;
-    margin: 20px;
-    border-bottom: 2px solid gray;
+export const ImgContainer = styled.div`
+  img {
+    width: 100%;
   }
 `;
