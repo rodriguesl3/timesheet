efc1792409ab6be14bf3899d8097711d8406c3b5
Lucas Rodrigues <l.rsouza@outlook.com>
2020-02-09 01:25:22 +0000

    style(main-page): adjust position of cards in main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,28 +1,33 @@
 import React, { FC } from 'react';
 import { MainContainer, ContentContainer } from './main.style';
 import SideBar from '../../components/Sidebar/Sidebar';
-import Project from '../../components/Projects';
+import News from '../../components/News';
 import { Card } from '../../index.style';
 import Wallet from '../../components/Wallet/Wallet';
 import StockSymbols from '../../components/StockSymbols/StockSymbols';
 
 const Main: FC = () => {
   return (
-    <ContentContainer>
+    <ContentContainer data-class="ContentContainer">
       <SideBar />
       <MainContainer>
-        <Card width={30} height={20}>
+        <Card width={70} height={20}>
           <StockSymbols />
         </Card>
-        <h1>Main Page</h1>
-        <div>HEADER</div>
-        <Card width={30} height={100}>
-          <Project title="What is going on" />
-        </Card>
-        <Card width={30} height={20}>
+        {/* <h1>Main Page</h1>
+        <div>HEADER</div> */}
+
+        <Card width={70} height={20}>
           <Wallet />
         </Card>
       </MainContainer>
+
+      <div className="side-bar-news">
+        {/* News... */}
+        <Card width={90} height={90}>
+          <News title="What is going on" />
+        </Card>
+      </div>
     </ContentContainer>
   );
 };
--- src/pages/Main/main.style.ts
+++ src/pages/Main/main.style.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 export const MainContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   flex-wrap: wrap;
-  width: 100%;
+  width: 70%;
 `;
 
 export const ContentContainer = styled.div`
@@ -12,4 +12,13 @@ export const ContentContainer = styled.div`
   flex-direction: row;
   background-color: #e9e9e9;
   width: 100%;
+
+  .side-bar-news {
+    width: 35%;
+  }
+`;
+
+export const NewsContainer = styled.div`
+  width: 30%;
+  height: 100vh;
 `;
