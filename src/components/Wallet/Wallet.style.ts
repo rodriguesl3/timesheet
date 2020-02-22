6147848fed4376a94ad89dafa64820cbd4622023
Lucas Rodrigues <l.rsouza@outlook.com>
2020-02-22 22:38:07 +0000

    style(main-page): adjust card position and sizes

--- src/components/Wallet/Wallet.style.ts
+++ src/components/Wallet/Wallet.style.ts
@@ -13,6 +13,23 @@ export const DetailConainer = styled.div`
   align-items: center;
 `;
 
+export const SpikeContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 12px;
+  border-bottom: 1px solid black;
+  padding-top: 10px;
+  padding-bottom: 10px;
+
+  .symbol-description {
+    padding-left: 12px;
+  }
+  .spike-arrow {
+    margin-left: 5px;
+  }
+`;
+
 export const WalletCompaniesLogos = styled.img`
   border: 2px solid;
   border-radius: 125px;
@@ -27,5 +44,6 @@ export const WalletCompanyName = styled.p`
 
 export const WalletTitle = styled.h1`
   margin: 20px;
+  text-align: center;
   /* justify-content: center; */
 `;
