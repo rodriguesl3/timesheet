b034049b6eef879d768ed24146e4db50fcfd3e83
Lucas Rodrigues <l.rsouza@outlook.com>
2020-02-25 08:33:45 +0000

    style(dashboard): add a header section

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { MainContainer, ContentContainer } from './main.style';
+import { MainContainer, ContentContainer, Col } from './main.style';
 import SideBar from '../../components/Sidebar/Sidebar';
 import News from '../../components/News';
 import { Card } from '../../index.style';
@@ -11,31 +11,44 @@ const Main: FC = () => {
   return (
     <>
       {/* <MainContainer>
-        <Card width={30} height={20}>
-          this is a widget
-        </Card>
+       <Card width={30} height={20}>
+            this is a widget
+          </Card>
       </MainContainer> */}
 
       <ContentContainer data-class="ContentContainer">
         <SideBar />
-        <MainContainer>
-          <Card width={40}>
-            <Wallet />
-          </Card>
-          <Card width={50} height={70}>
-            <Position />
-          </Card>
-          <Card width={95} height={20}>
-            <StockSymbols />
-          </Card>
-          <Card width={100}>top 30 assets with best performance</Card>
-        </MainContainer>
+        <Col>
+          <MainContainer>
+            <Card width={30} height={20}>
+              this is my current position value with a single chart with average price.
+            </Card>
+            <Card width={30} height={20}>
+              TBD
+            </Card>
+            <Card width={30} height={20}>
+              TBD
+            </Card>
+          </MainContainer>
+          <MainContainer>
+            <Card width={40}>
+              <Wallet />
+            </Card>
+            <Card width={50} height={70}>
+              <Position />
+            </Card>
+            <Card width={95} height={20}>
+              <StockSymbols />
+            </Card>
+            <Card width={100}>top 30 assets with best performance</Card>
+          </MainContainer>
+        </Col>
 
         <div className="side-bar-news">
           <Card width={80} height={90}>
             <News title="What is going on" />
           </Card>
-          <Card width={80}>this is another Card</Card>
+          <Card width={80}>twitter trends</Card>
         </div>
       </ContentContainer>
     </>
