557ab1bda35824baf207e95e70f60a024e0d91d2
rodriguesl3 <rodriguesl3@users.noreply.github.com>
2020-03-13 06:36:15 +0000

    deploy: 243243597bbef872a71a11090b86fab0d6d549e5

---
+++ precache-manifest.84a73e23d58d075bdb0283a04e939e38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfb3a0ddef1d1f5b562e5676aac8601",
+    "url": "/rodriguesl3/timesheet/index.html"
+  },
+  {
+    "revision": "a6e37773695f0a045e96",
+    "url": "/rodriguesl3/timesheet/static/css/2.24928661.chunk.css"
+  },
+  {
+    "revision": "a6e37773695f0a045e96",
+    "url": "/rodriguesl3/timesheet/static/js/2.2c91ef2a.chunk.js"
+  },
+  {
+    "revision": "ba197f2b6456a0c91a0ae7c8ffae89c9",
+    "url": "/rodriguesl3/timesheet/static/js/2.2c91ef2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fad543684add9c48309",
+    "url": "/rodriguesl3/timesheet/static/js/main.bff71ece.chunk.js"
+  },
+  {
+    "revision": "5ed6adf3b015be33a77a",
+    "url": "/rodriguesl3/timesheet/static/js/runtime-main.8dee7889.js"
+  },
+  {
+    "revision": "2babb95ce1ea72ba177a53133921342f",
+    "url": "/rodriguesl3/timesheet/static/media/New_York.2babb95c.png"
+  }
+]);
